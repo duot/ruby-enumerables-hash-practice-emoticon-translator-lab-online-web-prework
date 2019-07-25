8f60a4e8a20c3535dd7bfa6465549cc1ffa38102
optimistic-writer-7178 <duot.jim@gmail.com>
2019-07-25 08:40:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,5 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
-end
-
-def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
-  # code goes here
+def load_library file
+  YAML.load_file file
 end
